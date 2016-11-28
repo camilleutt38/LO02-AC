aef5e38c4331de85437ea29abc1b363f8c17a83a
Athena-Cataleya <lucie.cl.colin@gmail.com>
2016-11-28 19:26:32 +0100

    Scandale ca marche

--- src/LO02_projet/Joueur.java
+++ src/LO02_projet/Joueur.java
@@ -1,14 +1,39 @@
 package LO02_projet;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Joueur {
 
 	
 	private int num;
 	private int nbPrieres;
-	public Main main ;
+	public LinkedList<Croyant> main ;
+	private String nom;
 	
+	public Joueur(String nom){
+		this.nom = nom;
+		main = new LinkedList<Croyant>();
+	}
+	
+	public void prendreCroyant(Croyant croyant){
+		main.add(croyant);
+	}
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(nom);
+		sb.append(" : ");
+		sb.append(main);
+		sb.append("\n");
+		return sb.toString();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
 	
 	
--- src/LO02_projet/Partie.java
+++ src/LO02_projet/Partie.java
@@ -1,12 +1,17 @@
 package LO02_projet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+
+import TP04.Joueur;
 
 public class Partie {
 	
+	private ArrayList<Joueur> joueurs;
 	public Pile cartes;
 	
 	public Partie(){
+		joueurs = new ArrayList<Joueur>();
 		cartes = new Pile();
 		cartes.melanger();
 		}
@@ -17,6 +22,22 @@ public class Partie {
 	public String toString() {
 		return "Cartes" + cartes;
 	}
+	
+	public void ajouterJoueur(Joueur joueur) {
+		//if (partieEnCours == false) {
+			joueurs.add(joueur);
+		}
+	//}
+	
+	public void distribuerCarte() {
+		while (cartes.estVide() == false) {
+			Iterator<Joueur> it = joueurs.iterator();
+			while (it.hasNext()) {
+				Joueur j = (Joueur) it.next();
+				j.prendreCroyant(cartes.tirerCarteDessus());
+			}
+		}
+	}
 
 
 
@@ -25,6 +46,20 @@ public class Partie {
 		Partie p = new Partie();
 		System.out.println(p);
 		System.out.println(p.cartes.tirerCarteDessus());
+		
+		Joueur chaton = new Joueur("Chaton");
+		Joueur lucie = new Joueur("Lucie");
+		Joueur chris = new Joueur("Chris");
+		
+		p.ajouterJoueur(chaton);
+		p.ajouterJoueur(lucie);
+		p.ajouterJoueur(chris);
+		
+		p.distribuerCarte();
+		
+		System.out.println(chaton);
+		System.out.println(lucie);
+		System.out.println(chris);
 
 		
 
